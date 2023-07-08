b85b57641eeab5b4dee0467645de197db72e40b6
NS <121578862+oyoops@users.noreply.github.com>
2023-07-07 21:48:24 -0400

    Update agent.ts

--- src/agents/babycatagi/agent.ts
+++ src/agents/babycatagi/agent.ts
@@ -142,8 +142,7 @@ export class BabyCatAGI {
   // Tools functions
   async textCompletionTool(prompt: string) {
     if (getUserApiKey()) {
-      ////return await textCompletion(prompt, 'gpt-3.5-turbo', getUserApiKey());
-      return await textCompletion(prompt, 'gpt-4', getUserApiKey());
+      return await textCompletion(prompt, 'gpt-3.5-turbo', getUserApiKey());
     }
 
     const response = await axios
