9dac2264afa02540de593850ce5b424a38a1b479
NS <121578862+oyoops@users.noreply.github.com>
2023-07-07 21:47:25 -0400

    Update service.ts

--- src/agents/babycatagi/service.ts
+++ src/agents/babycatagi/service.ts
@@ -37,7 +37,7 @@ export const extractRelevantInfoAgent = async (
 ) => {
   const model = new OpenAI({
     openAIApiKey,
-    modelName: 'gpt-4',
+    modelName: 'gpt-3.5-turbo',
     temperature: 0.7,
     maxTokens: 800,
     n: 1,
